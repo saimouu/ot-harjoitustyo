6d31c17669f35b329fb607c832946d7fdb4bae9f
simo <49877847+saimouu@users.noreply.github.com>
2025-03-28 13:53:11 +0200

    Add move blocks left method

--- src/game/game_logic.py
+++ src/game/game_logic.py
@@ -27,6 +27,40 @@ class GameLogic:
                     return True
         return False
 
+    def _move_block_left(self, row, col):
+        # Wall check
+        if col - 1 < 0:
+            return
+
+        # Early return check
+        if (
+            self._grid[row][col - 1] != self._grid[row][col]
+            and self._grid[row][col - 1] != 0
+        ):
+            return
+
+        block_value = self._grid[row][col]
+        # traverse backwards through the row
+        for idx in range(col - 1, -1, -1):
+            current_block = self._grid[row][idx]
+            if current_block == 0:
+                self._grid[row][idx] = self._grid[row][idx + 1]
+                self._grid[row][idx + 1] = 0
+            elif current_block == block_value:
+                self._grid[row][idx] = block_value * 2
+                self._grid[row][idx + 1] = 0
+                break  # can only merge once
+            else:
+                break
+
+    def move_all_blocks_left(self):
+        for row in range(4):
+            for col in range(4):
+                # Zero-valued blocks are not moved
+                if self._grid[row][col] == 0:
+                    continue
+                self._move_block_left(row, col)
+
     @property
     def grid(self):
         return self._grid
--- src/main.py
+++ src/main.py
@@ -3,9 +3,16 @@ from game.game_logic import GameLogic
 
 def main():
     game = GameLogic()
+    game._grid[0][1] = 2
+    game._grid[0][0] = 2
+
+    game._grid[2][3] = 2
+    game._grid[2][0] = 2
+
+    game._grid[3][1] = 2
+    game._grid[1][0] = 2
     print(game)
-    game._spawn_random_block()
-    game._spawn_random_block()
+    game.move_all_blocks_left()
     print(game)
 
 
