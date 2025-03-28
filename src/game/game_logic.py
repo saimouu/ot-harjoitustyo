1d07341bd72c3e6763c668cc90eb2f5abd067d0a
simo <49877847+saimouu@users.noreply.github.com>
2025-03-28 12:42:19 +0200

    Add methods to game logic for spawning block to random location and for checking win condition

--- src/game/game_logic.py
+++ src/game/game_logic.py
@@ -1,6 +1,31 @@
+import random
+
+
 class GameLogic:
     def __init__(self):
         self._grid = [[0 for _ in range(4)] for _ in range(4)]
+
+    def _get_empty_spaces(self):
+        empty = []
+        for row in range(4):
+            for col in range(4):
+                if self._grid[row][col] == 0:
+                    empty.append((row, col))
+        return empty
+
+    def _spawn_random_block(self):
+        empty_spaces = self._get_empty_spaces()
+        space = random.choice(empty_spaces)
+        num = 2 if random.random() <= 0.9 else 4
+
+        self._grid[space[0]][space[1]] = num
+
+    def _check_win(self):
+        for row in range(4):
+            for col in range(4):
+                if self._grid[row][col] == 2048:
+                    return True
+        return False
 
     @property
     def grid(self):
--- src/main.py
+++ src/main.py
@@ -4,6 +4,9 @@ from game.game_logic import GameLogic
 def main():
     game = GameLogic()
     print(game)
+    game._spawn_random_block()
+    game._spawn_random_block()
+    print(game)
 
 
 if __name__ == "__main__":
